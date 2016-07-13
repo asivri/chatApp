38958874d243871f39fbc1be43d38ba640f0fb3e
Ahmet Sivri <asivri@fredonia.edu>
2016-07-13 15:19:00 +0300

    Fixed problems

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ var PORT = process.env.PORT || 3000;
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
-var socketio = require('socket.io')(http);
+var io = require('socket.io')(http);
 
 
 app.use(express.static(__dirname + '/public'));
 
 //TODO: Improve the code and add username
 //TODO: Fix below
-socketio.on('connection', function(){
+io.on('connection', function(){
   console.log('A user connected!');
 });
 
