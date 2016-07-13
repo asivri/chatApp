2bae27048c116732107720616896f6bbe8c888a1
Ahmet Sivri <asivri@fredonia.edu>
2016-07-13 15:06:54 +0300

    Added socket.io

--- server.js
+++ server.js
@@ -3,9 +3,16 @@ var PORT = process.env.PORT || 3000;
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var socketio = require('socket.io')(http);
 
 
 app.use(express.static(__dirname + '/public'));
+
+//TODO: Improve the code and add username
+//TODO: Fix below
+socketio.on('connection', function(){
+  console.log('A user connected!');
+});
 
 http.listen(PORT, function(){
   console.log('Listening');
